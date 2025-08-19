6d083a620d3689017b2a5e93562a66660c26e120
Your Name <guptaranjit939@gmail.com>
2025-08-19 15:29:42 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+
 
 import notesRoutes from "./routes/notesRoutes.js";
 import {connectDB} from "./config/db.js";
@@ -10,12 +12,16 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
 
 
 // middleware
+if(process.env.NODE_ENV !== "production"){
 app.use(cors({
     origin : "http://localhost:5173",
 }));
+}
+
 app.use(express.json()); // this middleware will parse JSON Bodies : req.body
 app.use(rateLimiter);
 
@@ -26,6 +32,14 @@ app.use(rateLimiter);
 // })
 
 app.use("/api/notes" , notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+});
+}
 
 connectDB().then(() => {
     app.listen(PORT , () => {
